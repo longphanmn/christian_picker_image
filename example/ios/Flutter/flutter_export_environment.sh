b44c2fa853486e2590ea804a6e1a4685fba8c8f3
TopDevZH <support@topdevzh.com>
2020-03-02 17:28:59 +0700

    Update glide

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Volumes/Data/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Data/Github/christian_picker_image/example"
+export "FLUTTER_ROOT=/Users/long/work/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/long/work/lphan/fundeal/camera/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Volumes/Data/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_FRAMEWORK_DIR=/Users/long/work/flutter/bin/cache/artifacts/engine/ios"
